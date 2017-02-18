8bd6098262ef7b9a38e786e9be612cb65c25893a
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-02-18 19:17:25 +0200

    updating

---
+++ src/vendor.js
@@ -0,0 +1,4 @@
+//ref to libs by webpack in the production build
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,19 +1,46 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
 
 export default {
 
   devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
+    //generate external css file with a hash in the file name
+    new ExtractTextPlugin('[name].[contenthash].css'),
+    //create html file the includes ref to bundle js
+    new HtmlWebpackPlugin({
+      template:'src/index.html',
+      minify: {
+        removeComments:true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+
+      },
+      inject: true,
+      //properties you define here are available in index.html
+      //using htmlwebpackplugin.options.varname
+      trackJSToken:'43ad216f57d94259968435894490a5c7'
+    }),
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
 

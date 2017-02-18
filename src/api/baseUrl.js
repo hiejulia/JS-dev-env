0814c424bea69daf6c4f9507f8dea5919b5ff867
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-02-18 14:39:55 +0200

    create baseUrl

---
+++ src/api/baseUrl.js
@@ -0,0 +1,6 @@
+export default function getBaseUrl() {
+  const inDev = window.location.hostname === 'localhost';
+  return inDev ? 'http://localhost:3001':'/';
+
+}
+
--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,11 +1,24 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 //export
 export function getUsers(){
   return get('users');
 
+}
+
+//function get
+function get(url){
+  return fetch(baseUrl+ url).then(onSuccess,onError);
+}
 
 
+
+
+export function deleteUser(id){
+  return del(`users/${id}`);
 }
 
 //success

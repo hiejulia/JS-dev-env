7e87d6de17f943bda22794d98be6b635a42510a3
HienNguyen711 <hienminhnguyen711@gmail.com>
2017-02-18 12:53:19 +0200

    set up first test

--- src/index.test.js
+++ src/index.test.js
@@ -1,2 +1,24 @@
 import {expect} from 'chai';
-describe()
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+//write test here
+describe('First test',() => {
+  it('passed' () => {
+    expect(true).to.equal(true);
+  });
+});
+
+//test index.html
+describe('Test index.html',() => {
+  it('h1 has users',(done) => {
+    const index = fs.readFileSync('./src/index.html',"utf-8");
+    jsdom.env(index,function(err,window){
+      const h1 = window.document.getElementsByTagName('h1')[0].innerHTML;
+      expect(h1).to.equal("Users");
+      done();
+      window.close();
+    });
+
+  })
+});
